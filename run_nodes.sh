a2cf2267dc8d6de77ed0f4d790dbb11514a808ae
Syed-MuhammadTaha <simam.bscs22seecs@seecs.edu.pk>
2025-05-16 00:23:53 +0500

    Reset git tracking and add .gitignore

--- run_nodes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start three nodes in the background
-NODE_ID=node_1 /usr/local/bin/python3 main.py &
-NODE_ID=node_2 /usr/local/bin/python3 main.py &
-NODE_ID=node_3 /usr/local/bin/python3 main.py &
-
-# Wait for all background processes
-wait 
